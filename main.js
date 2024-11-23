031a0b337b4f5cd3b29422b31365a9af16c5016c
kasraalitabar <kasraalitabar@gmail.com>
2024-11-23 21:34:04 +0330

    make ok

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
 const boxes = document.querySelectorAll(".box-items");
-const selectedboxes = []
+const selectedBoxes = []
 
-function boxclick(evt){
-    if(selectedboxes.length === 0){
+function boxclick(evt) {
+    if (selectedBoxes.length === 0) {
+        evt.target.classList.remove("hidden")
+        selectedBoxes.push(evt.target)
+    } else {
         evt.target.classList.remove("hidden");
-        selectedboxes.push(evt.target);
+        selectedBoxes.push(evt.target);
+        if (selectedBoxes[0].innerHtml === selectedBoxes[1].innerHtml) {
+            selectedBoxes.length = 0;
+        } else {
+            setTimeout(function () {
+                selectedBoxes[0].classList.add("hidden");
+                selectedBoxes[1].classList.add("hidden");
+                selectedBoxes.length = 0;
+            },1000)
+        }
     }
 }
 
-function hideALL(){
+function hideALL() {
     for (const box of boxes) {
         box.classList.add("hidden")
-    }   
+    }
 }
-function setTimeouthandeler(){
+function setTimeouthandeler() {
     hideALL();
-} 
+}
 
-setTimeout(hideALL,5000)
+setTimeout(hideALL, 5000)
 
-for(const box of boxes){
+for (const box of boxes) {
     box.addEventListener("click", boxclick)
 }
